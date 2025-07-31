21d77c152f72175efdbd75188ae4d0089e80fa09
Nicolas Brueckner <74188620+NicolasBrueckner@users.noreply.github.com>
2025-07-31 12:19:36 +0200

    orbit info in UI
    still needs a lot of refinement

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,5 +1,6 @@
 #region
 
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -15,9 +16,21 @@ public class UIController : MonoBehaviour
 	public Button hideButton;
 	public SequenceList sequences;
 
+	[ Header( "Information Labels" ) ]
+	public TMP_Text positionLabel;
+
+	public TMP_Text velocityLabel;
+	public TMP_Text massLabel;
+	public TMP_Text freeGroupLabel;
+	public TMP_Text periodLabel;
+	public TMP_Text energyLabel;
+	public TMP_Text dateLabel;
+
+
 	private void Start()
 	{
 		RuntimeEventManager.FileLoaded += OnFileLoaded;
+		RuntimeEventManager.OrbitInfoLoaded += OnInfoLoaded;
 
 		sequenceDropdown.onValueChanged.AddListener( OnSequenceChanged );
 		orbitDropdown.onValueChanged.AddListener( OnOrbitChanged );
@@ -26,6 +39,11 @@ public class UIController : MonoBehaviour
 		sequenceDropdown.AddOptions( sequences.sequences );
 
 		OnSequenceChanged( 0 );
+	}
+
+	private void OnInfoLoaded( OrbitInformation obj )
+	{
+		WriteInformation( obj );
 	}
 
 	private void OnOrbitChanged( int index )
@@ -43,5 +61,18 @@ public class UIController : MonoBehaviour
 	{
 		orbitDropdown.ClearOptions();
 		orbitDropdown.AddOptions( loader.GetKeys() );
+	}
+
+	private void WriteInformation( OrbitInformation obj )
+	{
+		positionLabel.text = "Initial positions: [" +
+		                     string.Join( ", ", obj.initialPositions.Select( v => $"({v.x}, {v.y})" ) ) + "]";
+		velocityLabel.text = "Initial velocities: [" +
+		                     string.Join( ", ", obj.initialVelocities.Select( v => $"({v.x}, {v.y})" ) ) + "]";
+		massLabel.text = $"Masses: {obj.masses}";
+		freeGroupLabel.text = $"Free group element: {obj.freeGroupElement}";
+		periodLabel.text = $"Period: {obj.period}";
+		energyLabel.text = $"Energy: {obj.energy}";
+		dateLabel.text = $"Date discovered: {obj.year}";
 	}
 }
