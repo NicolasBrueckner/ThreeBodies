49ccbe347c8ec366a8025bcc2563ab393b8f593b
Nicolas Brueckner <74188620+NicolasBrueckner@users.noreply.github.com>
2025-09-24 10:15:09 +0200

    camera distance fix

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,6 +1,5 @@
 #region
 
-using System.Linq;
 using UnityEngine;
 
 #endregion
@@ -9,6 +8,8 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 	public float scale = 1f;
+	public float horizontalPadding;
+	public float verticalPadding;
 	private Camera _cam;
 
 	private void Start()
@@ -19,7 +20,38 @@ public class CameraController : MonoBehaviour
 
 	private void OnOrbitCalculated( CalculationResult result )
 	{
-		float maxValue = result.positions.AsParallel().Max();
-		_cam.orthographicSize = maxValue * scale;
+		float minX = float.MaxValue;
+		float minY = float.MaxValue;
+		float maxX = float.MinValue;
+		float maxY = float.MinValue;
+
+		float[] positions = result.positions;
+
+		for( int i = 0; i < positions.Length; i += 2 )
+		{
+			float x = positions[ i ];
+			float y = positions[ i + 1 ];
+
+			if( x < minX ) minX = x;
+			if( x > maxX ) maxX = x;
+			if( y < minY ) minY = y;
+			if( y > maxY ) maxY = y;
+		}
+
+		minX -= horizontalPadding;
+		maxX += horizontalPadding;
+		minY -= verticalPadding;
+		maxY += verticalPadding;
+
+		Vector2 center = new( ( minX + maxX ) * 0.5f, ( minY + maxY ) * 0.5f );
+		Vector2 size = new( maxX - minX, maxY - minY );
+
+		_cam.transform.position = new( center.x, center.y, _cam.transform.position.z );
+
+		float aspect = Screen.width / ( float )Screen.height;
+		float verticalSize = size.y * 0.5f;
+		float horizontalSize = size.x * 0.5f / aspect;
+
+		_cam.orthographicSize = Mathf.Max( verticalSize, horizontalSize ) * scale;
 	}
 }
