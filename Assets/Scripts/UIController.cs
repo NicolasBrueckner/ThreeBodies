205a3ed3dd22e68051f66660697dae14ae92daa2
Nicolas Brueckner <74188620+NicolasBrueckner@users.noreply.github.com>
2025-09-29 13:27:51 +0200

    ui improvements

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -87,15 +87,17 @@ public class UIController : MonoBehaviour
 
 	private void WriteInformation( OrbitInformation obj )
 	{
-		positionLabel.text = "Initial positions: [" +
-		                     string.Join( ", ", obj.initialPositions.Select( v => $"({v.x}, {v.y})" ) ) + "]";
-		velocityLabel.text = "Initial velocities: [" +
-		                     string.Join( ", ", obj.initialVelocities.Select( v => $"({v.x}, {v.y})" ) ) + "]";
-		massLabel.text = "Masses: [" +
-		                 string.Join( ", ", obj.masses.Select( v => $"({v})" ) ) + "]";
-		freeGroupLabel.text = $"Free group element: {obj.freeGroupElement}";
-		periodLabel.text = $"Period: {obj.period}";
-		energyLabel.text = $"Energy: {obj.energy}";
-		dateLabel.text = $"Date discovered: {obj.year}";
+		positionLabel.text = "Initial positions:\n" +
+		                     string.Join( "\n",
+			                     obj.initialPositions.Select( v => $"( {v.x,+6:0.000}, {v.y,+6:0.000} )" ) );
+		velocityLabel.text = "Initial velocities:\n" +
+		                     string.Join( "\n",
+			                     obj.initialVelocities.Select( v => $"( {v.x,+6:0.000}, {v.y,+6:0.000} )" ) );
+		massLabel.text = "Masses:\n" +
+		                 string.Join( ", ", obj.masses.Select( v => $"{v}" ) );
+		freeGroupLabel.text = $"Free group element:\n{obj.freeGroupElement}";
+		periodLabel.text = $"Period:\n{obj.period}s";
+		energyLabel.text = $"Energy:\n{obj.energy}";
+		dateLabel.text = $"Date discovered:\n{obj.year}";
 	}
 }
